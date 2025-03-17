141dfb869c4a2919f940ea331842cec612433ffd
Rudra Sharan <rudrasharan45@gmail.com>
2025-03-17 22:41:41 +0530

    Pagination add

--- backend/src/views/Products.vue
+++ backend/src/views/Products.vue
@@ -58,6 +58,34 @@
 
       </tbody>
       </table>
+      <div class="flex justify-between items-center mt-5">
+        <span>
+          Showing from 1{{ products.form }} to {{ products.to }}
+        </span>
+        <nav
+        v-if="products.total > products.limit"
+        class="relative z-0 inline-flex justify-center rounded-md shadow-sm -space-x-px"
+        aria-label="Pagination">
+
+        <a 
+          v-for="(link,i) of products.links"
+          :key="i" :disabled="!link.url"
+          href="#" @click="getForPage($event,link)"
+          aria-current="page"
+          class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
+          :class="[
+            link.active
+            ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+            : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+            i == 0 ? 'rounded-l-md':'',
+            i == products.links.length -1 ? 'rounded-r-md':'',
+            !link.url? 'bg-gray-100 text-gray-700':''
+          ]"
+          v-html="link.label"
+          ></a>
+
+        </nav>
+      </div>
     </template>
 </div>
 </template>
@@ -79,6 +107,7 @@ onMounted(()=>{
 function getProducts(){
   store.dispatch('getProducts')
 }
+
 
 </script>
 
