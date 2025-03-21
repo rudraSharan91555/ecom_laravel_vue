5a661669303333c9df368d8f22369a67d0b2f744
Rudra Sharan <rudrasharan45@gmail.com>
2025-03-21 15:06:54 +0530

    Product Add new product

--- backend/src/views/Products/ProductModal.vue
+++ backend/src/views/Products/ProductModal.vue
@@ -1,24 +1,39 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <TransitionRoot as="template" :show="show">
-    <Dialog as="div" class="relative z-10" @close="show = false">
-      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
-                       leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
-        <div class="fixed inset-0 bg-black bg-opacity-70 transition-opacity"/>
-      </TransitionChild>
-
-      <div class="fixed z-10 inset-0 overflow-y-auto">
-        <div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
-          <TransitionChild as="template" enter="ease-out duration-300"
-                           enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                           enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
-                           leave-from="opacity-100 translate-y-0 sm:scale-100"
-                           leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            <DialogPanel
-              class="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-[700px] sm:w-full">
+    
+    <TransitionRoot appear :show="show" as="template">
+      <Dialog as="div" @close="closeModal" class="relative z-10">
+        <TransitionChild
+          as="template"
+          enter="duration-300 ease-out"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="duration-200 ease-in"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div class="fixed inset-0 bg-black/25" ></div>
+        </TransitionChild>
+  
+        <div class="fixed inset-0 overflow-y-auto">
+          <div
+            class="flex min-h-full items-center justify-center p-4 text-center"
+          >
+            <TransitionChild
+              as="template"
+              enter="duration-300 ease-out"
+              enter-from="opacity-0 scale-95"
+              enter-to="opacity-100 scale-100"
+              leave="duration-200 ease-in"
+              leave-from="opacity-100 scale-100"
+              leave-to="opacity-0 scale-95"
+            >
+              <DialogPanel
+                class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+              >
               <Spinner v-if="loading"
-                       class="absolute left-0 top-0 bg-white right-0 bottom-0 flex items-center justify-center"/>
-              <header class="py-3 px-4 flex justify-between items-center">
+                class="absolute left-0 top-0 bg-white right-0 flex items-center justify-center"/>             
+
+              <header class="py-3 px-4 flex justify-between item-center">
                 <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900">
                   {{ product.id ? `Update product: "${props.product.title}"` : 'Create new Product' }}
                 </DialogTitle>
@@ -26,7 +41,7 @@
                   @click="closeModal()"
                   class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]"
                 >
-                  <svg
+                <svg
                     xmlns="http://www.w3.org/2000/svg"
                     class="h-6 w-6"
                     fill="none"
@@ -40,7 +55,7 @@
                       d="M6 18L18 6M6 6l12 12"
                     />
                   </svg>
-                </button>
+              </button>
               </header>
               <form @submit.prevent="onSubmit">
                 <div class="bg-white px-4 pt-5 pb-4">
@@ -61,54 +76,65 @@
                   </button>
                 </footer>
               </form>
-            </DialogPanel>
-          </TransitionChild>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
         </div>
-      </div>
-    </Dialog>
-  </TransitionRoot>
-</template>
-
-<script setup>
-import {computed, onUpdated, ref} from 'vue'
-import {Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot} from '@headlessui/vue'
-import CustomInput from "../../components/core/CustomInput.vue";
+      </Dialog>
+    </TransitionRoot>
+  </template>
+  
+  <script setup>
+import { ref, computed,onUpdated } from 'vue'
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
+import Spinner from '../../components/core/Spinner.vue'
+import CustomInput from '../../components/core/CustomInput.vue'
 import store from "../../store/index.js";
-import Spinner from "../../components/core/Spinner.vue";
 
-const product = ref({
-  id: props.product.id,
-  title: props.product.title,
-  image: props.product.image,
-  description: props.product.description,
-  price: props.product.price
-})
+
 
 const loading = ref(false)
 
 const props = defineProps({
   modelValue: Boolean,
-  product: {
-    required: true,
-    type: Object,
-  }
+  product:{
+        required:true,
+        type:Object,
+        
+    }
 })
 
-const emit = defineEmits(['update:modelValue', 'close'])
-
-const show = computed({
-  get: () => props.modelValue,
-  set: (value) => emit('update:modelValue', value)
-})
-
-onUpdated(() => {
-  product.value = {
+const product = ref({
     id: props.product.id,
     title: props.product.title,
     image: props.product.image,
     description: props.product.description,
     price: props.product.price
-  }
+})
+
+
+
+const emit = defineEmits(['update:modelValue','close'])
+
+const show = computed({
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value) 
+})
+
+onUpdated(()=>{
+    product.value = {
+        id: props.product.id,
+        title: props.product.title,
+        image: props.product.image,
+        description: props.product.description,
+        price: props.product.price
+    }
 })
 
 function closeModal() {
@@ -144,4 +170,6 @@ function onSubmit() {
       })
   }
 }
+
+
 </script>
