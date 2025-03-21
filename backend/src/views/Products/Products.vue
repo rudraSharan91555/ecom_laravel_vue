58af3370d66fba25db661977690e84f1052ed246
Rudra Sharan <rudrasharan45@gmail.com>
2025-03-21 12:40:33 +0530

    Modal Set

--- backend/src/views/Products/Products.vue
+++ backend/src/views/Products/Products.vue
@@ -1,17 +1,26 @@
 <template>
 <div class="flex item-center justify-between mb-3">                
     <h1 class="text-3xl font-semibold text-gray-800 mb-4">Products</h1>
-    <button type="submit" class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+    <button type="submit" 
+      @click="showProductModal"
+      class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
         Add new Product
     </button>
 </div>
+<ProductModal v-model="showModal"/>
 <ProductsTable/>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import ProductModal from './ProductModal.vue';
 import ProductsTable from './ProductsTable.vue';
 
-                       
+const showModal = ref(false)
+
+function showProductModal(){
+  showModal.value = true
+}
 </script>
 
 <style scoped>
